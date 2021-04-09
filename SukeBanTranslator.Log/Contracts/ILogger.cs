ca206d5992443c4b642242031193b83d741e22ba
SukeBan <76999918+SukeBann@users.noreply.github.com>
2021-04-09 19:17:44 +0800

    文件夹命名变动

---
+++ SukeBanTranslator.Log/Contracts/ILogger.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace SukeBanTranslator.Log
+{   
+    /// <summary>
+    /// 提供日志的本地文件存储功能
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// 记录一条信息级别的日志消息
+        /// </summary>
+        /// <param name="message">消息内容。</param>
+        void Info(string message);
+
+        /// <summary>
+        /// 记录一条信息级别的日志消息
+        /// </summary>
+        /// <param name="exception">要记录的信息，将收集此信息的消息，栈堆跟踪等信息 </param>
+        void Info(Exception exception);
+
+        /// <summary>
+        /// 记录一条信息级别的日志消息
+        /// </summary>
+        /// <param name="exception">要记录的信息信息，将收集此信息的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        void Info(Exception exception, string messageTemplate);
+
+        /// <summary>
+        /// 记录一条信息级别的日志消息
+        /// </summary>
+        /// <param name="exception">要记录的信息信息，将收集此信息的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        /// <param name="args">消息格式化字符串使用的参数。</param>
+        void Info(Exception exception, string messageTemplate, params object[] args);
+
+        /// <summary>
+        /// 记录一条警告级别的日志消息
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        void Warning(string message);
+
+        /// <summary>
+        /// 记录一条警告级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的警告信息，将收集此警告的消息、堆栈跟踪等信息。</param>
+        void Warning(Exception exception);
+
+        /// <summary>
+        /// 记录一条警告级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的警告信息，将收集此警告的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        void Warning(Exception exception, string messageTemplate);
+
+        /// <summary>
+        /// 记录一条警告级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的警告信息，将收集此警告的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        /// <param name="args">消息格式化字符串使用的参数。</param>
+        void Warning(Exception exception, string messageTemplate, params object[] args);
+
+
+        /// <summary>
+        /// 记录一条错误级别的日志消息。
+        /// </summary>
+        /// <param name="message">消息内容。</param>
+        void Error(string message);
+
+        /// <summary>
+        /// 记录一条错误级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的错误信息，将收集此错误的消息、堆栈跟踪等信息。</param>
+        void Error(Exception exception);
+
+        /// <summary>
+        /// 记录一条错误级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的错误信息，将收集此错误的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        void Error(Exception exception, string messageTemplate);
+
+        /// <summary>
+        /// 记录一条错误级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的错误信息，将收集此错误的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        /// <param name="args">消息格式化字符串使用的参数。</param>
+        void Error(Exception exception, string messageTemplate, params object[] args);
+
+        /// <summary>
+        /// 记录一条调试级别的日志消息。
+        /// </summary>
+        /// <param name="message">消息内容。</param>
+        void Debug(string message);
+
+        /// <summary>
+        /// 记录一条调试级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的调试信息，将收集此调试的消息、堆栈跟踪等信息。</param>
+        void Debug(Exception exception);
+
+        /// <summary>
+        /// 记录一条调试级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的调试信息，将收集此调试的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        void Debug(Exception exception, string messageTemplate);
+
+        /// <summary>
+        /// 记录一条调试级别的日志消息。
+        /// </summary>
+        /// <param name="exception">要记录的调试信息，将收集此调试的消息、堆栈跟踪等信息。</param>
+        /// <param name="messageTemplate">消息的格式化字符串，参考Serilog的使用文档。</param>
+        /// <param name="args">消息格式化字符串使用的参数。</param>
+        void Debug(Exception exception, string messageTemplate, params object[] args);
+    }
+}
---
+++ SukeBanTranslator.Log/Implements/LoggerImpl.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Serilog;
+using Serilog.Core;
+using SukeBanTranslator.Shared;
+
+namespace SukeBanTranslator.Log
+{   
+    [Export(typeof(ILogger))]
+    class LoggerImpl:ILogger
+    {
+        #region Fields
+        private static Logger _logger;
+        #endregion
+
+        #region Ctor
+        public LoggerImpl()
+        {
+            var logFilePath = Environments.LogFilePath;
+
+            _logger = new LoggerConfiguration()
+                .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+        #endregion
+
+        #region Methods
+
+        public void Info(string message)
+        {
+            Log("info", message);
+        }
+
+        public void Info(Exception exception)
+        {
+            Log("info", exception, null);
+        }
+
+        public void Info(Exception exception, string messageTemplate)
+        {
+            Log("info", exception, messageTemplate);
+        }
+
+        public void Info(Exception exception, string messageTemplate, params object[] args)
+        {
+            Log("info", exception, messageTemplate, args);
+        }
+
+        public void Warning(string message)
+        {
+            Log("warning", message);
+        }
+
+        public void Warning(Exception exception)
+        {
+            Log("warning", exception, null);
+        }
+
+        public void Warning(Exception exception, string messageTemplate)
+        {
+            Log("warning", exception, messageTemplate);
+        }
+
+        public void Warning(Exception exception, string messageTemplate, params object[] args)
+        {
+            Log("warning", exception, messageTemplate, args);
+        }
+
+        public void Debug(string message)
+        {
+            Log("trace", message);
+        }
+
+        public void Debug(Exception exception)
+        {
+            Log("trace", exception, null);
+        }
+
+        public void Debug(Exception exception, string messageTemplate)
+        {
+            Log("trace", exception, messageTemplate);
+        }
+
+        public void Debug(Exception exception, string messageTemplate, params object[] args)
+        {
+            Log("trace", exception, messageTemplate, args);
+        }
+
+        public void Error(string message)
+        {
+            Log("error", message);
+        }
+
+        public void Error(Exception exception)
+        {
+            Log("error", exception, null);
+        }
+
+        public void Error(Exception exception, string messageTemplate)
+        {
+            Log("error", exception, messageTemplate);
+        }
+
+        public void Error(Exception exception, string messageTemplate, params object[] args)
+        {
+            Log("error", exception, messageTemplate, args);
+        }
+
+        #endregion
+
+        #region Functions
+
+        private void Log(string level, string message)
+        {
+            switch (level)
+            {
+                case "info":
+                    _logger.Information(message);
+                    break;
+                case "warning":
+                    _logger.Information(message);
+                    break;
+                case "error":
+                    _logger.Information(message);
+                    break;
+                case "trace":
+                    _logger.Debug(message);
+                    break;
+            }
+        }
+
+        private void Log(string level, Exception exception, string messageTemplate, params object[] args)
+        {
+            switch (level)
+            {
+                case "info":
+                    _logger.Information(exception, messageTemplate, args);
+                    break;
+                case "warning":
+                    _logger.Information(exception, messageTemplate, args);
+                    break;
+                case "error":
+                    _logger.Information(exception, messageTemplate, args);
+                    break;
+                case "trace":
+                    _logger.Debug(exception, messageTemplate, args);
+                    break;
+            }
+        }
+
+        #endregion
+
+    }
+}
